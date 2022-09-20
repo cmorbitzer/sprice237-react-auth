f65a543bea7401811079a6e9919f663e70e1f351
Chris Morbitzer <chris.morbitzer@gmail.com>
2022-09-19 21:57:19 -0500

    Add useUpdateEmail hook

--- src/firebase/firebaseAuthContextHooks.ts
+++ src/firebase/firebaseAuthContextHooks.ts
@@ -5,6 +5,7 @@ import {
   confirmPasswordReset,
   createUserWithEmailAndPassword,
   linkWithCredential,
+  reauthenticateWithCredential,
   sendEmailVerification,
   sendPasswordResetEmail,
   signInWithEmailAndPassword,
@@ -12,18 +13,22 @@ import {
   signInWithRedirect,
   signOut,
   unlink,
+  updateEmail,
   verifyPasswordResetCode,
   AuthCredential,
+  EmailAuthProvider,
   UserCredential,
-} from 'firebase/auth';
-import { useCallback } from 'react';
-import { useFirebaseAuthContext } from './firebaseAuthContext';
+} from "firebase/auth";
+import { useCallback } from "react";
+import { useFirebaseAuthContext } from "./firebaseAuthContext";
 
 type OmitFirstArg<F> = F extends (x: never, ...args: infer P) => infer R
   ? (...args: P) => R
   : never;
 
-const wrapFirebaseAuthFunction = <F extends (firebaseAuth: Auth, ...args: never[]) => unknown>(
+const wrapFirebaseAuthFunction = <
+  F extends (firebaseAuth: Auth, ...args: never[]) => unknown
+>(
   firebaseAuth: Auth,
   func: F
 ): OmitFirstArg<F> => {
@@ -43,8 +48,9 @@ const useWrappedFirebaseAuthFunction = <
 export const useApplyActionCode = (): OmitFirstArg<typeof applyActionCode> =>
   useWrappedFirebaseAuthFunction(applyActionCode);
 
-export const useConfirmPasswordReset = (): OmitFirstArg<typeof confirmPasswordReset> =>
-  useWrappedFirebaseAuthFunction(confirmPasswordReset);
+export const useConfirmPasswordReset = (): OmitFirstArg<
+  typeof confirmPasswordReset
+> => useWrappedFirebaseAuthFunction(confirmPasswordReset);
 
 export const useCreateUserWithEmailAndPassword = (): OmitFirstArg<
   typeof createUserWithEmailAndPassword
@@ -58,7 +64,7 @@ export const useLinkWithCredential = (): ((
   return useCallback(
     (credential: AuthCredential) => {
       if (!user) {
-        throw new Error('User is not valid');
+        throw new Error("User is not valid");
       }
       return linkWithCredential(user, credential);
     },
@@ -71,26 +77,53 @@ export const useSendEmailVerification = (): (() => Promise<void>) => {
 
   return useCallback(() => {
     if (!user) {
-      throw new Error('User is not valid');
+      throw new Error("User is not valid");
     }
     return sendEmailVerification(user);
   }, [user]);
 };
 
-export const useSendPasswordResetEmail = (): OmitFirstArg<typeof sendPasswordResetEmail> =>
-  useWrappedFirebaseAuthFunction(sendPasswordResetEmail);
+export const useSendPasswordResetEmail = (): OmitFirstArg<
+  typeof sendPasswordResetEmail
+> => useWrappedFirebaseAuthFunction(sendPasswordResetEmail);
 
-export const useSignInWithEmailAndPassword = (): OmitFirstArg<typeof signInWithEmailAndPassword> =>
-  useWrappedFirebaseAuthFunction(signInWithEmailAndPassword);
+export const useSignInWithEmailAndPassword = (): OmitFirstArg<
+  typeof signInWithEmailAndPassword
+> => useWrappedFirebaseAuthFunction(signInWithEmailAndPassword);
 
 export const useSignInWithPopup = (): OmitFirstArg<typeof signInWithPopup> =>
   useWrappedFirebaseAuthFunction(signInWithPopup);
 
-export const useSignInWithRedirect = (): OmitFirstArg<typeof signInWithRedirect> =>
-  useWrappedFirebaseAuthFunction(signInWithRedirect);
+export const useSignInWithRedirect = (): OmitFirstArg<
+  typeof signInWithRedirect
+> => useWrappedFirebaseAuthFunction(signInWithRedirect);
 
 export const useSignOut = (): OmitFirstArg<typeof signOut> =>
   useWrappedFirebaseAuthFunction(signOut);
+
+export const useUpdateEmail = (): ((
+  currentEmail: string,
+  newEmail: string
+) => Promise<void>) => {
+  const { user } = useFirebaseAuthContext();
+
+  return useCallback(
+    async (currentEmail: string, newEmail: string) => {
+      if (!user) {
+        throw new Error("User is not valid");
+      }
+
+      if (!user.email) {
+        throw new Error("User does not have an email address");
+      }
+
+      const credential = EmailAuthProvider.credential(user.email, currentEmail);
+      await reauthenticateWithCredential(user, credential);
+      return updateEmail(user, newEmail);
+    },
+    [user]
+  );
+};
 
 export const useUnlink = (): ((providerId: string) => Promise<User>) => {
   const { user } = useFirebaseAuthContext();
@@ -98,7 +131,7 @@ export const useUnlink = (): ((providerId: string) => Promise<User>) => {
   return useCallback(
     (providerId: string) => {
       if (!user) {
-        throw new Error('User is not valid');
+        throw new Error("User is not valid");
       }
       return unlink(user, providerId);
     },
@@ -106,5 +139,6 @@ export const useUnlink = (): ((providerId: string) => Promise<User>) => {
   );
 };
 
-export const useVerifyPasswordResetCode = (): OmitFirstArg<typeof verifyPasswordResetCode> =>
-  useWrappedFirebaseAuthFunction(verifyPasswordResetCode);
+export const useVerifyPasswordResetCode = (): OmitFirstArg<
+  typeof verifyPasswordResetCode
+> => useWrappedFirebaseAuthFunction(verifyPasswordResetCode);
